a025c1b80c614a2966421e7b4fa012e3316548ae
Tobias Skov Pedersen <tubspot@hotmail.com>
2021-06-03 10:40:18 +0200

    matmul done

--- Tests_matmul/plot_matmul.py
+++ Tests_matmul/plot_matmul.py
@@ -32,46 +32,62 @@ no_sensor_points_list = np.array([9, 36, 64, 100, 144, 225, 400, 900,
 #plt.savefig(path + "\\" + "rfluxmtx.png", dpi=500, bbox_inches = 'tight', pad_inches = 0)
 
 
-x = list(range(17))
+x = list(range(16))
 
 plt.figure(figsize=(6,3),dpi=500)
-plt.plot(radiance_timings[:,0], label = "dctimestep")
-plt.plot(radiance_timings[:,1], label = "rmtxop")
+plt.plot(radiance_timings[:16,0], label = "dctimestep")
+plt.plot(radiance_timings[:16,1], label = "rmtxop")
 plt.xlabel("Number of sensor points", fontsize=12)
 plt.ylabel("Walltime [s]", fontsize=12)
 plt.title("Radiance native functions runtime", fontsize=14)
 plt.grid()
 plt.legend()
-plt.xticks(x, labels=no_sensor_points_list[0:17], rotation = 70, fontsize=12)
+plt.xticks(x, labels=no_sensor_points_list[0:16], rotation = 70, fontsize=12)
 plt.show()
 
 #%% numpy
 
-x = list(range(18))
+x = list(range(16))
 
 plt.figure(figsize=(6,3),dpi=500)
-plt.plot(numpy_timings[:,0], label = "reading dc matrix")
-plt.plot(numpy_timings[:,1], label = "reading sky matrix")
-plt.plot(numpy_timings[:,2], label = "matmul")
-plt.plot(numpy_timings[:,3], label = "scale")
-plt.plot(numpy_timings[:,4], label = "save result matrix")
+plt.plot(numpy_timings[:16,0], label = "reading dc matrix")
+plt.plot(numpy_timings[:16,1], label = "reading sky matrix")
+plt.plot(numpy_timings[:16,2], label = "matmul")
+plt.plot(numpy_timings[:16,3], label = "scale")
+plt.plot(numpy_timings[:16,4], label = "save result matrix")
 plt.xlabel("Number of sensor points", fontsize=12)
 plt.ylabel("Walltime [s]", fontsize=12)
 plt.title("numpy runtime", fontsize=14)
 plt.grid()
 plt.legend()
-plt.xticks(x, labels=no_sensor_points_list[0:18], rotation = 70, fontsize=12)
+plt.xticks(x, labels=no_sensor_points_list[0:16], rotation = 70, fontsize=12)
 plt.show()
 
 #%% numpy without save results
 
+x = list(range(16))
+
+plt.figure(figsize=(6,3),dpi=500)
+plt.plot(numpy_timings[:16,0], label = "reading dc matrix")
+plt.plot(numpy_timings[:16,1], label = "reading sky matrix")
+plt.plot(numpy_timings[:16,2], label = "matmul")
+plt.plot(numpy_timings[:16,3], label = "scale")
+plt.xlabel("Number of sensor points", fontsize=12)
+plt.ylabel("Walltime [s]", fontsize=12)
+plt.title("numpy runtime", fontsize=14)
+plt.grid()
+plt.legend()
+plt.xticks(x, labels=no_sensor_points_list[0:16], rotation = 70, fontsize=12)
+plt.show()
+
+#%% numpy without save results and read dc
+
 x = list(range(18))
 
 plt.figure(figsize=(6,3),dpi=500)
-plt.plot(numpy_timings[:,0], label = "reading dc matrix")
-plt.plot(numpy_timings[:,1], label = "reading sky matrix")
-plt.plot(numpy_timings[:,2], label = "matmul")
-plt.plot(numpy_timings[:,3], label = "scale")
+plt.plot(numpy_timings[:16,1], label = "reading sky matrix")
+plt.plot(numpy_timings[:16,2], label = "matmul")
+plt.plot(numpy_timings[:16,3], label = "scale")
 plt.xlabel("Number of sensor points", fontsize=12)
 plt.ylabel("Walltime [s]", fontsize=12)
 plt.title("numpy runtime", fontsize=14)
@@ -80,20 +96,35 @@ plt.legend()
 plt.xticks(x, labels=no_sensor_points_list[0:18], rotation = 70, fontsize=12)
 plt.show()
 
+#%% numpy without save results and read dc and sky matrix
+
+x = list(range(18))
+
+plt.figure(figsize=(6,3),dpi=500)
+plt.plot(numpy_timings[:16,1], label = "reading sky matrix")
+plt.plot(numpy_timings[:16,2], label = "matmul")
+plt.plot(numpy_timings[:16,3], label = "scale")
+plt.xlabel("Number of sensor points", fontsize=12)
+plt.ylabel("Walltime [s]", fontsize=12)
+plt.title("numpy runtime", fontsize=14)
+plt.grid()
+plt.legend()
+plt.xticks(x, labels=no_sensor_points_list[0:18], rotation = 70, fontsize=12)
+plt.show()
 
 #%% cupy
 
 x = list(range(16))
 
 plt.figure(figsize=(6,3),dpi=500)
-plt.plot(cupy_timings[:,0], label = "reading dc matrix")
-plt.plot(cupy_timings[:,1], label = "reading sky matrix")
-plt.plot(cupy_timings[:,2], label = "copy dc matrix")
-plt.plot(cupy_timings[:,3], label = "copy sky matrix")
-plt.plot(cupy_timings[:,4], label = "matmul")
-plt.plot(cupy_timings[:,5], label = "scale")
-plt.plot(cupy_timings[:,6], label = "copy results matrix")
-plt.plot(cupy_timings[:,7], label = "save result matrix")
+plt.plot(cupy_timings[:16,0], label = "reading dc matrix")
+plt.plot(cupy_timings[:16,1], label = "reading sky matrix")
+plt.plot(cupy_timings[:16,2], label = "copy dc matrix")
+plt.plot(cupy_timings[:16,3], label = "copy sky matrix")
+plt.plot(cupy_timings[:16,4], label = "matmul")
+plt.plot(cupy_timings[:16,5], label = "scale")
+plt.plot(cupy_timings[:16,6], label = "copy results matrix")
+plt.plot(cupy_timings[:16,7], label = "save result matrix")
 plt.xlabel("Number of sensor points", fontsize=12)
 plt.ylabel("Walltime [s]", fontsize=12)
 plt.title("cupy runtime", fontsize=14)
@@ -107,13 +138,13 @@ plt.show()
 x = list(range(16))
 
 plt.figure(figsize=(6,3),dpi=500)
-plt.plot(cupy_timings[:,0], label = "reading dc matrix")
-plt.plot(cupy_timings[:,1], label = "reading sky matrix")
-plt.plot(cupy_timings[:,2], label = "copy dc matrix")
-plt.plot(cupy_timings[:,3], label = "copy sky matrix")
-plt.plot(cupy_timings[:,4], label = "matmul")
-plt.plot(cupy_timings[:,5], label = "scale")
-plt.plot(cupy_timings[:,6], label = "copy results matrix")
+plt.plot(cupy_timings[:16,0], label = "reading dc matrix")
+plt.plot(cupy_timings[:16,1], label = "reading sky matrix")
+plt.plot(cupy_timings[:16,2], label = "copy dc matrix")
+plt.plot(cupy_timings[:16,3], label = "copy sky matrix")
+plt.plot(cupy_timings[:16,4], label = "matmul")
+plt.plot(cupy_timings[:16,5], label = "scale")
+plt.plot(cupy_timings[:16,6], label = "copy results matrix")
 plt.xlabel("Number of sensor points", fontsize=12)
 plt.ylabel("Walltime [s]", fontsize=12)
 plt.title("cupy runtime", fontsize=14)
@@ -123,19 +154,50 @@ plt.xticks(x, labels=no_sensor_points_list[0:16], rotation = 70, fontsize=12)
 plt.show()
 
 
+#%% cupy without save results and without reading matrices
+x = list(range(16))
+
+plt.figure(figsize=(6,3),dpi=500)
+plt.plot(cupy_timings[:16,2], label = "copy dc matrix")
+plt.plot(cupy_timings[:16,3], label = "copy sky matrix")
+plt.plot(cupy_timings[:16,4], label = "matmul")
+plt.plot(cupy_timings[:16,5], label = "scale")
+plt.plot(cupy_timings[:16,6], label = "copy results matrix")
+plt.xlabel("Number of sensor points", fontsize=12)
+plt.ylabel("Walltime [s]", fontsize=12)
+plt.title("cupy runtime", fontsize=14)
+plt.grid()
+plt.legend()
+plt.xticks(x, labels=no_sensor_points_list[0:16], rotation = 70, fontsize=12)
+plt.show()
+
+
+
 #%% cupy vs numpy 
 
 
 x = list(range(16))
 
 plt.figure(figsize=(6,3),dpi=500)
-plt.plot(cupy_timings[:,2], label = "cupy - copy dc matrix")
-plt.plot(cupy_timings[:,3], label = "cupy - copy sky matrix")
-plt.plot(cupy_timings[:,4], label = "cupy - matmul")
-plt.plot(cupy_timings[:,5], label = "cupy - scale")
-plt.plot(cupy_timings[:,6], label = "cupy - copy results matrix")
-plt.plot(numpy_timings[:16,2], label = "numpy - matmul")
-plt.plot(numpy_timings[:16,3], label = "numpy - scale")
+plt.plot(cupy_timings[:16,2] + cupy_timings[:,3] + cupy_timings[:,6], label = "cupy - copy op.")
+plt.plot(cupy_timings[:16,4] + cupy_timings[:,5], label = "cupy - matrix op.")
+plt.plot(numpy_timings[:16,2] + numpy_timings[:16,3], label = "numpy - matrix op.")
+plt.xlabel("Number of sensor points", fontsize=12)
+plt.ylabel("Walltime [s]", fontsize=12)
+plt.title("cupy vs numpy runtime", fontsize=14)
+plt.grid()
+plt.legend()
+plt.xticks(x, labels=no_sensor_points_list[0:16], rotation = 70, fontsize=12)
+plt.show()
+
+#%% cupy vs numpy 
+
+
+x = list(range(16))
+
+plt.figure(figsize=(6,3),dpi=500)
+plt.plot(cupy_timings[:16,2] + cupy_timings[:,3] + cupy_timings[:,6] + cupy_timings[:,4] + cupy_timings[:,5], label = "cupy - copy op. and matrix op.")
+plt.plot(numpy_timings[:16,2] + numpy_timings[:16,3], label = "numpy - matrix op.")
 plt.xlabel("Number of sensor points", fontsize=12)
 plt.ylabel("Walltime [s]", fontsize=12)
 plt.title("cupy vs numpy runtime", fontsize=14)
@@ -155,7 +217,7 @@ plt.plot(radiance_timings.sum(axis=1)[:16], label = "Radiance total runtime")
 plt.plot(numpy_timings.sum(axis=1)[:16], label = "numpy total runtime")
 plt.plot(cupy_timings.sum(axis=1), label = "cupy total runtime")
 plt.xlabel("Number of sensor points", fontsize=12)
-plt.ylabel("Walltime [s]", fontsize=12)
+plt.ylabel("Wall time [s]", fontsize=12)
 plt.title("Total runtime", fontsize=14)
 plt.grid()
 plt.legend()
@@ -194,6 +256,22 @@ plt.plot(cupy_short, label = "cupy total runtime")
 plt.xlabel("Number of sensor points", fontsize=12)
 plt.ylabel("Walltime [s]", fontsize=12)
 plt.title("Total runtime", fontsize=14)
+plt.grid()
+plt.legend()
+plt.xticks(x, labels=no_sensor_points_list[0:16], rotation = 70, fontsize=12)
+plt.show()
+
+
+#%%
+
+
+x = list(range(16))
+
+plt.figure(figsize=(6,3),dpi=500)
+plt.plot(radiance_timings.sum(axis=1)[:16]/numpy_timings.sum(axis=1)[:16], label = "numpy speed up")
+plt.xlabel("Number of sensor points", fontsize=12)
+plt.ylabel("Speedup", fontsize=12)
+plt.title("Speedup of matrix operations in numpy compared to Radiance", fontsize=14)
 plt.grid()
 plt.legend()
 plt.xticks(x, labels=no_sensor_points_list[0:16], rotation = 70, fontsize=12)
